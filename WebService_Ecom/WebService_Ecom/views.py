c856c2b6e9d24bb51d0d47a02abb9c1a7b130ef2
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2025-02-26 03:10:34 +0530

    valid commit
    changed views

--- WebService_Ecom/WebService_Ecom/views.py
+++ WebService_Ecom/WebService_Ecom/views.py
@@ -4,11 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def index(request):
-    print("here123")
-    if request.method == "POST":
-        print("post")
-        print("a: ", request.GET.get('a'))
-        print("b: ", request.GET.get('b'))
-        print("c: ", request.GET.get("c"))
-        print("d: ", request.GET.get("d"))
+    if request.method == "GET":
+        print("query: ", request.GET.get('query'))
     return render(request, "WebService_Ecom/index.html")
