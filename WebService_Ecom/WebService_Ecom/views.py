d6452be44c3ed4b8507cb85764294c05c0692dad
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2025-02-26 03:14:42 +0530

    Delete WebService_Ecom directory

--- WebService_Ecom/WebService_Ecom/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
-
-@csrf_exempt
-def index(request):
-    if request.method == "GET":
-        print("query: ", request.GET.get('query'))
-    return render(request, "WebService_Ecom/index.html")
